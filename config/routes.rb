1c17296966dcc5871306670f22c3c050601f8696
Nishimurashun <132205509+sHuN945@users.noreply.github.com>
2023-09-10 13:48:22 +0000

    [update]travel_review

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,33 @@
 Rails.application.routes.draw do
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  scope module: :public do
+    root to: 'homes#top'
+    resources :comments, only: [:show, :index, :destroy]
+    get 'customers/unsubscribe' => "customers#unsubscribe", as: :unsubscribe
+    patch 'customers/withdrawal' => "customers#withdrawal"
+    get 'customers', to: 'customers#show'
+    get 'customers/edit', to: "customers#edit"
+    patch 'customers', to: "customers#update"
+    resources :posts, only: [:create, :destroy, :update, :index, :new, :show]
+    resources :reviews, only: [:show, :edit, :update, :index, :new, :create, :destroy]
+    get '/about' => "homes#about"
+  end
+  
+  
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  namespace :admin do
+    get '/' => "homes#top"
+    resources :reviews, only: [:show, :index, :destroy]
+    resources :customers, only: [:edit, :update, :show, :index]
+  end
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
