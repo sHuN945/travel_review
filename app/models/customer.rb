8fc40e9a4f90eb7119030f9ceccb34f6a419d11e
Nishimurashun <132205509+sHuN945@users.noreply.github.com>
2023-09-27 00:05:07 +0000

    [update]travel_review

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,18 +5,19 @@ class Customer < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :comments, dependent: :destroy
   has_many :posts, dependent: :destroy
-         
+
   def full_name
     self.last_name + " " + self.first_name
   end
-  
+
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |customer|
       customer.password = SecureRandom.urlsafe_base64
     end
   end
-  
+
   validates :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, presence: true
-  
-  
+
+
+
 end
